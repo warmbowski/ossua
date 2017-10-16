a7ab2cb76aa8794c27df389a812c7f939ea5babe
Paul Laskowski <paul@warmbowski.org>
2017-10-15 18:17:25 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Ossua from './Ossua';
+import './App.css';
+
+// const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div> */}
+        <Ossua />
+        {/* <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Ossua.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as L from 'leaflet';
+import './Ossua.css';
+import '../node_modules/leaflet/dist/leaflet.css';
+
+class Ossua extends React.Component {
+  componentDidMount() {
+    const map = L.map('map_ossua', {
+      crs: L.CRS.Simple,
+    });
+    const bounds: L.LatLngBoundsExpression = [[0, 0], [1000, 1000]];
+    L.imageOverlay('ossua.jpg', bounds).addTo(map);
+
+    map.fitBounds(bounds);
+  }
+
+  render() {
+    return <div id="map_ossua" />;
+  }
+}
+
+export default Ossua;
